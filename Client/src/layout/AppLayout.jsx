922328dc18454cffcb720ef857f86280f574f147
Riyat121 <145189781+Riyat121@users.noreply.github.com>
2026-02-10 02:17:19 +0530

    layout setup

--- Client/src/layout/AppLayout.jsx
+++ Client/src/layout/AppLayout.jsx
@@ -6,18 +6,18 @@ export default function AppLayout({ children }) {
   return (
     <div className="h-screen flex flex-col">
       
-      {/* Top Navbar */}
+      
       <TopNavbar />
 
       <div className="flex flex-1 overflow-hidden">
         
-        {/* Left Icon Sidebar */}
+      
         <IconSidebar />
 
-        {/* Ticket Views Sidebar */}
+        
         <TicketViewsSidebar />
 
-        {/* Main Content */}
+        
         <div className="flex flex-1">{children}</div>
 
       </div>
