2fe10bdaa9794d907863a3c1ec3b78df29024a45
Riyat121 <145189781+Riyat121@users.noreply.github.com>
2026-02-10 02:36:32 +0530

    added ticket id

--- Client/src/components/tickets/TicketList.jsx
+++ Client/src/components/tickets/TicketList.jsx
@@ -1,27 +1,24 @@
-export default function TicketList() {
+import TicketItem from "./TicketItem";
+
+export default function TicketList({
+  tickets,
+  selectedTicket,
+  setSelectedTicket
+}) {
+
   return (
-    <div className="w-96 border-r bg-white p-4">
-      
-      <input
-        placeholder="Search tickets"
-        className="w-full border p-2 rounded mb-4"
-      />
+    <div className="w-[ 360px] bg-white border-r">
 
-      {[1,2,3,4,5].map(ticket => (
-        <div
-          key={ticket}
-          className="p-3 border-b hover:bg-gray-50 cursor-pointer"
-        >
-          <div className="font-medium">
-            Laudantium neque veritatis
-          </div>
-
-          <div className="text-sm text-gray-500">
-            APPS-216 • To Do
-          </div>
-        </div>
+      {tickets.map(ticket => (
+        <TicketItem
+          key={ticket.id}
+          ticket={ticket}
+          isSelected={selectedTicket?.id === ticket.id}
+          onClick={() => setSelectedTicket(ticket)}
+        />
       ))}
 
     </div>
   );
 }
+ 
--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import TicketList from "../components/tickets/TicketList";
 import TicketDetails from "../components/ticketDetails/TicketDetails";
-import RightSidebar from "../components/ticketDetails/RightSidebar";
+import TicketViewsSidebar from "../components/sidebar/TicketViewsSidebar";
 
 export default function Dashboard() {
+
+  const [selectedTicket, setSelectedTicket] = useState(null);
+
+  const tickets = [
+    { id: 1, title: "Laudantium neque veritatis", code: "OPS-102" },
+    { id: 2, title: "Molestiae saepe illum", code: "APPS-216" },
+    { id: 3, title: "Dignissimos maiores porro", code: "APPS-216" },
+  ];
+
   return (
-    <>
-      <TicketList />
-      <TicketDetails />
-      <RightSidebar />
-    </>
+    <div className="flex flex-1">
+
+      <TicketViewsSidebar />
+
+      <TicketList
+        tickets={tickets}
+        selectedTicket={selectedTicket}
+        setSelectedTicket={setSelectedTicket}
+      />
+
+      <TicketDetails ticket={selectedTicket} />
+
+    </div>
   );
 }
